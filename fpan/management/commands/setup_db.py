2d86c2431f37b1bdc841b0d9cab7e56eca388098
adam cox <mr.adamcox@gmail.com>
2017-10-13 18:15:40 -0500

    add function to build default set of user groups and test users

--- fpan/management/commands/setup_db.py
+++ fpan/management/commands/setup_db.py
@@ -1,12 +1,11 @@
 from django.core import management
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.conf import settings
 import psycopg2 as db
-import os
+import os, string, random, csv
 from arches.db.install import truncate_db
 from arches.management.commands.packages import Command as packages
-
 
 class Command(BaseCommand):
     help = 'drops and recreates the app database.'
@@ -20,13 +19,12 @@ class Command(BaseCommand):
             default=False,
             help='forces the continuation of any command that has a confirmation prompt',
         )
-        pass
 
     def handle(self, *args, **options):
 
         force = options['yes']
         self.setup_db(force)
-
+        self.build_fpan_groups()
         
     def setup_db(self,force=False):
         
@@ -84,6 +82,47 @@ class Command(BaseCommand):
         
         # print("admin superuser created. password = cspmaster.")
         
+    def build_fpan_groups(self):
+    
+        print "\nREMOVING UNECESSARY GROUPS\n-----------------------"
+        keep_groups = ["Graph Editor","Resource Editor","RDM Administrator","Guest"]
+        all_groups = Group.objects.filter()
+        for g in all_groups:
+            if not g.name in keep_groups:
+                print "  removing group: "+g.name
+                g.delete()
         
+        print "\nADDING FPAN GROUPS AND TEST USERS\n----------------"
+        groups_users = {
+            "State Park":["TomokaStatePark","LakeLouisaStatePark"],
+            "FL BAR":["fl_bar_user"],
+            "FMSF":["fmsf_user"],
+            "FWC":["fwc_user"],
+            "FL Forestry":["fl_forestry_user"],
+            "FL Aquatic Preserve":["fl_aquatic_preserve_user"],
+            "Scout":["scout_user"]
+        }
         
-        print("done")
+        print "  creating groups and default users...",
+        outlist = []
+        for group, userlist in groups_users.iteritems():
+            print "  creating group: "+group
+            newgroup = Group.objects.get_or_create(name=group)[0]
+            
+            for user in userlist:
+                pw = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
+                try:
+                    existing_user = User.objects.get(username=user)
+                    existing_user.delete()
+                except:
+                    pass
+                print "    -- adding new user: {} ({})".format(user,pw)                    
+                newuser = User.objects.create_user(user,"",pw)
+                newuser.groups.add(newgroup)
+                outlist.append((user,pw))
+                
+        with open(os.path.join(settings.SECRET_LOG,"initial_user_info.csv"),"wb") as outcsv:
+            write = csv.writer(outcsv)
+            write.writerow(("username","password"))
+            for user_pw in outlist:
+                write.writerow(user_pw)
