c5535b7304d3b33a7ae597f61f95fe91ac08070b
adam cox <mr.adamcox@gmail.com>
2017-10-30 15:50:41 -0500

    create spatial join function

--- fpan/media/js/views/components/functions/spatial-join.js
+++ fpan/media/js/views/components/functions/spatial-join.js
@@ -7,35 +7,55 @@ function (ko, koMapping, ListView, FunctionViewModel, chosen) {
     return ko.components.register('views/components/functions/spatial-join', {
         viewModel: function(params) {
             FunctionViewModel.apply(this, arguments);
-            console.log("Running...")
             var self = this;
-            // var nodegroups = {};
+            
+            // create a ko observable that can be used by the select2 widgets
+            // note that the widget needs to be configured to record the correct
+            // property (nodeid, nodegroup_id, etc.)
             this.nodes = ko.observableArray();
-            
-            
-            this.spatial_node_id = params.config.spatial_node_id;
-            this.triggering_nodegroups = params.config.triggering_nodegroups;
-            this.spatial_node_id.subscribe(function(ng){
-              console.log('this is run because a new value is set for the spatial_node_id variable', ng);
-              self.config.triggering_nodegroups = [ng];
-            })
-
-            this.table_name = params.config.table_name;
-            this.table_name.subscribe(function(ng){});
-            
-            this.field_name = params.config.field_name;
-            this.field_name.subscribe(function(ng){});
-            
-            this.target_node_id = params.config.target_node_id;
-            this.target_node_id.subscribe(function(ng){});
-            
             this.graph.nodes.forEach(function(node){
-                console.log(node);
                 this.nodes.push(node);
             }, this);
             
+            // set triggering nodegroup variable so it can be updated below
+            // to run a function, you must provide at least one nodegroup_id
+            // to this variable. note that it must be a nodegroup_id, not just
+            // a nodeid.
+            this.triggering_nodegroups = params.config.triggering_nodegroups;
+
+            // set spatial node id from existing params
+            this.spatial_node_id = params.config.spatial_node_id;
             
-            console.log(params);
+            // and subscribe it so it updates automatically as the select2
+            // widget is used. note that the widget must reference this variable
+            // name. the function may be empty, but in this case we need to use
+            // it to get the nodegroup_id of the selected node and then update
+            // the triggering_nodegroup config variable. 
+            this.spatial_node_id.subscribe(function(ng){
+                
+                // 
+                var node_array = self.nodes()
+                node_array.forEach(function(node){
+                  if (ng == node.nodeid) {
+                      self.config.triggering_nodegroups = [node.nodegroup_id];
+                  }
+              });
+            })
+
+            // the following three sections are simpler versions of above; set
+            // a variable based on previous config value, and subscribe it to
+            // the input widget that references this variable name
+            this.table_field_one = params.config.table_field_one;
+            this.table_field_one.subscribe(function(ng){});
+            
+            this.target_node_id_one = params.config.target_node_id_one;
+            this.target_node_id_one.subscribe(function(ng){});
+            
+            this.table_field_two = params.config.table_field_two;
+            this.table_field_two.subscribe(function(ng){});
+            
+            this.target_node_id_two = params.config.target_node_id_two;
+            this.target_node_id_two.subscribe(function(ng){});
 
             window.setTimeout(function(){$("select[data-bind^=chosen]").trigger("chosen:updated")}, 300);
         },
