122b797e44673e52d3f3cc776623dde3586a86cd
billy <wewill147@gmail.com>
2017-11-17 14:14:47 -0600

    remove url endpoint for scout profile

--- hms/urls.py
+++ hms/urls.py
@@ -4,14 +4,7 @@ from django.contrib.gis import admin
 from . import views
 
 urlpatterns = [
-    url(r'^scouts/$', views.scouts_dropdown, name='scouts_dropdown'),
-    url(r'^scout/(?P<username>\w+)/', views.scout_profile, name='scout_profile'),
+    url(r'^scouts/$', views.scouts_dropdown, name='scouts_dropdown'),    
 ]
 
-
-    # 
-    # 
-    # url(r'^scout/signup', views.scout_signup, name='scout_signup'),
-    # url(r'^regions/$', views.show_regions, name='show_regions'),
-    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
-    #     views.activate, name='activate'),
+# url(r'^scout/(?P<username>\w+)/', views.scout_profile, name='scout_profile'),
