307d986a011119eda301c02713bf5137eab71cff
adam cox <mr.adamcox@gmail.com>
2017-12-05 20:53:22 -0600

    adjust function to handle superuser admins too

--- fpan/utils/context_processors.py
+++ fpan/utils/context_processors.py
@@ -2,7 +2,11 @@ from django.conf import settings
     
 def user_type(request):
     state_groups = ['FL Aquatic Preserve','FL BAR','FL Forestry','FMSF','FWC','State Park']
+    state_user = request.user.groups.filter(name__in=state_groups).exists()
+    if request.user.is_superuser:
+        state_user = False
     return {
-        'user_is_state': request.user.groups.filter(name__in=state_groups).exists(),
-        'user_is_scout': request.user.groups.filter(name='Scout').exists()
+        'user_is_state': state_user,
+        'user_is_scout': request.user.groups.filter(name='Scout').exists(),
+        'user_is_admin': request.user.is_superuser
     }
