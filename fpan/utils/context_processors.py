f8c6bd85bbc504ebe3917e03cc82202c2395326c
adam cox <mr.adamcox@gmail.com>
2017-12-11 11:45:09 -0600

    modify context processor to use existing functions

--- fpan/utils/context_processors.py
+++ fpan/utils/context_processors.py
@@ -1,12 +1,9 @@
 from django.conf import settings
+from .accounts import check_state_access,check_scout_access
     
 def user_type(request):
-    state_groups = ['FL Aquatic Preserve','FL BAR','FL Forestry','FMSF','FWC','State Park']
-    state_user = request.user.groups.filter(name__in=state_groups).exists()
-    if request.user.is_superuser:
-        state_user = False
     return {
-        'user_is_state': state_user,
-        'user_is_scout': request.user.groups.filter(name='Scout').exists(),
+        'user_is_state': check_state_access(request.user),
+        'user_is_scout': check_scout_access(request.user),
         'user_is_admin': request.user.is_superuser
     }
