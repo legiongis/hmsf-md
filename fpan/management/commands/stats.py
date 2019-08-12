4fbcb13de3dc0f10a7730f69bd875f5fbc016b35
adam cox <mr.adamcox@gmail.com>
2019-08-11 20:30:01 -0500

    add initial stats collecting command

---
+++ fpan/management/commands/stats.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import json
+from django.core import management
+from django.core.management.base import BaseCommand, CommandError
+from arches.app.models.resource import Resource
+from arches.app.models.models import Node, NodeGroup
+from arches.app.models.graph import Graph
+from arches.app.models.tile import Tile
+
+class Command(BaseCommand):
+
+    help = 'collects stats needed for FPAN year-end reporting'
+
+    def add_arguments(self, parser):
+        pass
+        # parser.add_argument("uuid",help='input the uuid string to find')
+
+    def handle(self, *args, **options):
+
+        report_nodes = Node.objects.filter(name="Scout Report")
+
+        for node in report_nodes:
+            graph = Graph.objects.get(graphid=node.graph_id)
+            if graph.name == "Scout Report":
+                continue
+            print graph.name
+            report_ng = NodeGroup.objects.get(nodegroupid=node.nodegroup_id)
+            report_tiles = Tile.objects.filter(nodegroup_id=report_ng)
+            resources = [r.resourceinstance_id for r in report_tiles]
+            
+            print "total reports:", len(report_tiles)
+            print "resource count:", len(set(resources))
+            for tile in report_tiles:
+                child_tiles = Tile.objects.filter(parenttile_id=tile.tileid)
