c064c747c005785e396efbeb1c705dd9d2b6b499
adam cox <mr.adamcox@gmail.com>
2019-10-22 00:14:18 -0500

    big improvement and refactor
    Signed-off-by: adam cox <mr.adamcox@gmail.com>

--- fpan/management/commands/update_managed_area_nodes.py
+++ fpan/management/commands/update_managed_area_nodes.py
@@ -3,6 +3,8 @@ from arches.app.models.models import Node, NodeGroup, Value, Concept
 from arches.app.models.tile import Tile
 from arches.app.models.concept import Concept as NonORMConcept
 
+RESOURCE_TO_USE = "f1575fda-6983-4a77-a016-5021dab7697d"
+
 class Command(BaseCommand):
 
     help = 'bulk addition of user accounts from a csv file'
@@ -23,85 +25,100 @@ class Command(BaseCommand):
                tiles in place to use the new conceptsfor each managed area instead of the existing
                string values in those tiles.
         '''
-
-        self.convert_managed_area_category_nodes("Managed Area Category")
-        self.convert_nodes_and_update_tiles("Managing Agency")
-        self.convert_nodes_and_update_tiles("Managed Area Name")
-
-    def convert_managed_area_category_nodes(self, collection_name):
-        '''this is essentially the same as convert_nodes_and_update_tiles() below,
-        but is kept separate at this time becuase additional logic will need to be
-        added to handle the new State Park regional grouping.'''
         
-        collection_nodes = Concept.objects.filter(nodetype_id="Collection")
-        for cn in collection_nodes:
-            labels = NonORMConcept().get(cn.conceptid).values
-            if collection_name in [l.value for l in labels]:
-                collection = cn
+        self.set_nodes_to_concept_list()
+        self.convert_tile_values()
 
-        # get the real category nodes (they will be used to find the tiles)
+    def convert_tile_values(self):
+        
+        cat_nodes = Node.objects.filter(name="Managed Area Category")
+        cat_nodeids = [str(i.nodeid) for i in cat_nodes]
+        ma_nodes = Node.objects.filter(name="Managing Agency")
+        ma_nodeids = [str(i.nodeid) for i in ma_nodes]
+        man_nodes = Node.objects.filter(name="Managed Area Name")
+        man_nodeids = [str(i.nodeid) for i in man_nodes]
+        
+        allnodeids = cat_nodeids + ma_nodeids + man_nodeids
+        nodes = list(cat_nodes) + list(ma_nodes) + list(man_nodes)
+        ngs = list(set([i.nodegroup_id for i in nodes]))
+        
+        for ngid in ngs:
+            ng = NodeGroup.objects.get(nodegroupid=ngid)
+            tiles = Tile.objects.filter(nodegroup_id=ng.nodegroupid)
+            if tiles.count() == 0:
+                continue
+            print("GRAPH: " + str(tiles[0].resourceinstance_id))
+            print(str(len(tiles))+ " tiles to update")
+            for i, t in enumerate(tiles):
+
+                for k, v in t.data.items():
+
+                    if v is None or k not in allnodeids:
+                        continue
+
+                    if v == "State Park":
+                        ## this is where the region spreadsheets will need to be referenced,
+                        ## to put this site in a state park region. For now just make the
+                        ## slight change so that it fits the existing label in the RDM
+                        v = "State Parks"
+
+                    try:
+                        newval = Value.objects.get(value=v).valueid
+                    except Value.DoesNotExist:
+                        print("ERROR: {} RESOURCE ID: {}".format(str(v), t.resourceinstance_id))
+                        continue
+
+                    t.data[k] = [str(newval)]
+
+                t.save(log=False)
+                
+                if i % 500 == 0:
+                    print i
+                elif i % 50 == 0:
+                    print i,
+
+                
+    def set_nodes_to_concept_list(self):
+        
+        cat_coll, ma_coll, man_coll = None, None, None
+        collections = Concept.objects.filter(nodetype_id="Collection")
+        for cn in collections:
+            labels = NonORMConcept().get(cn.conceptid).values
+            if "Managed Area Category" in [l.value for l in labels]:
+                cat_coll = cn
+            elif "Managing Agency" in [l.value for l in labels]:
+                ma_coll = cn
+            elif "Managed Area Name" in [l.value for l in labels]:
+                man_coll = cn
+
         ma_cat_nodes = Node.objects.filter(name="Managed Area Category")
-
         for n in ma_cat_nodes:
-
-            # alter the datatype of the node and assign the RDM collection
             n.datatype = "concept-list"
-            n.config = {"rdmCollection":str(collection.conceptid)}
+            n.config = {"rdmCollection":str(cat_coll.conceptid)}
             n.save()
 
-            # now find and convert all existing values for this node
-            # ng = NodeGroup.objects.get(nodegroupid=n.nodegroup_id)
-            # print ng.__dict__
-            tiles = Tile.objects.filter(nodegroup_id=n.nodegroup_id)
-            print("modifying {} tiles".format(len(tiles)))
-            for t in tiles:
-                exval = t.data[str(n.nodeid)]
-                if exval is None:
-                    continue
-                if exval == "State Park":
-                    ## this is where the region spreadsheets will need to be referenced,
-                    ## to put this site in a state park region. For now just make the
-                    ## slight change so that it fits the existing label in the RDM
-                    exval = "State Parks"
-                try:
-                    newval = Value.objects.get(value=exval).valueid
-                except Value.DoesNotExist:
-                    print("ERROR: " + exval)
-                    continue
-                t.data[str(n.nodeid)] = [str(newval)]
-                t.save()
-
-    def convert_nodes_and_update_tiles(self, collection_name):
-    
-        collection_nodes = Concept.objects.filter(nodetype_id="Collection")
-        for cn in collection_nodes:
-            labels = NonORMConcept().get(cn.conceptid).values
-            if collection_name in [l.value for l in labels]:
-                collection = cn
-
-        # get the real category nodes (they will be used to find the tiles)
-        ma_nodes = Node.objects.filter(name=collection_name)
-
+        ma_nodes = Node.objects.filter(name="Managing Agency")
         for n in ma_nodes:
-
-            # alter the datatype of the node and assign the RDM collection
             n.datatype = "concept-list"
-            n.config = {"rdmCollection":str(collection.conceptid)}
+            n.config = {"rdmCollection":str(ma_coll.conceptid)}
             n.save()
 
-            # now find and convert all existing values for this node
-            # ng = NodeGroup.objects.get(nodegroupid=n.nodegroup_id)
-            # print ng.__dict__
-            tiles = Tile.objects.filter(nodegroup_id=n.nodegroup_id)
-            print("modifying {} tiles".format(len(tiles)))
-            for t in tiles:
-                exval = t.data[str(n.nodeid)]
-                if exval is None:
-                    continue
-                try:
-                    newval = Value.objects.get(value=exval).valueid
-                except Value.DoesNotExist:
-                    print "ERROR:", exval
-                    continue
-                t.data[str(n.nodeid)] = [str(newval)]
-                t.save()
+        man_nodes = Node.objects.filter(name="Managed Area Name")
+        for n in man_nodes:
+            n.datatype = "concept-list"
+            n.config = {"rdmCollection":str(man_coll.conceptid)}
+            n.save()
+
+    def reset_nodes_to_string(self):
+
+        cat_nodes = Node.objects.filter(name="Managed Area Category")
+        ma_nodes = Node.objects.filter(name="Managing Agency")
+        man_nodes = Node.objects.filter(name="Managed Area Name")
+        
+        for nodeset in [cat_nodes, ma_nodes, man_nodes]:
+            for n in nodeset:
+                n.datatype = 'string'
+        # if not n.datatype == "concept-list":
+            # n.datatype = "concept-list"
+            # n.config = {"rdmCollection":str(collection.conceptid)}
+                n.save()
