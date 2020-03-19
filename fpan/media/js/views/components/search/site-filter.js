28761c1b014559faef335f2480fa236b64d2d803
adam cox <mr.adamcox@gmail.com>
2020-03-19 18:42:56 -0500

    overhaul to site-filter search component, injected to url on every search

--- fpan/media/js/views/components/search/site-filter.js
+++ fpan/media/js/views/components/search/site-filter.js
@@ -1,35 +1,39 @@
 define([
     'knockout',
-    'views/components/search/base-filter'
-], function(ko, BaseFilter) {
+    'views/components/search/base-filter',
+    'fpan'
+], function(ko, BaseFilter, FPAN) {
     var componentName = 'site-filter';
+    console.log("at the top of the file");
     return ko.components.register(componentName, {
         viewModel: BaseFilter.extend({
             initialize: function(options) {
                 options.name = 'Site Filter';
+
+                console.log("initializing site filter");
                 BaseFilter.prototype.initialize.call(this, options);
 
-                this.filter = ko.observableArray();
-
-                // this is where the filter gets added to the list of available filters
-                this.filters[componentName](this);
-
-                // this is so if you refresh the page the url parameter is restored
                 this.restoreState();
             },
 
+            // this doesn't seem to be used anywhere and can likely be removed.
             updateQuery: function() {
+                console.log("updating query");
                 var queryObj = this.query();
+                queryObj[componentName] = 'enabled';
                 this.query(queryObj);
             },
 
             restoreState: function() {
-                var query = this.query();
+                console.log("restoring state");
+                var queryObj = this.query();
+                if (FPAN.full_site_access == "True") {
+                  queryObj[componentName] = 'disabled';
+                } else {
+                  queryObj[componentName] = 'enabled';
+                }
+                this.query(queryObj);
             },
-
-            clear: function() {
-                this.filter.removeAll();
-            }
 
         }),
         template: { require: 'text!templates/views/components/search/site-filter.htm' }
--- fpan/search/components/site_filter.py
+++ fpan/search/components/site_filter.py
@@ -1,6 +1,13 @@
+import os
+import json
+import logging
 from arches.app.utils.betterJSONSerializer import JSONDeserializer
 from arches.app.search.elasticsearch_dsl_builder import Bool, Terms
 from arches.app.search.components.base import BaseSearchFilter
+from arches.app.models.system_settings import settings
+from fpan.utils.search_filter import apply_advanced_docs_permissions
+
+logger = logging.getLogger(__name__)
 
 details = {
     "searchcomponentid": "",
@@ -8,7 +15,7 @@ details = {
     "icon": "",
     "modulename": "site_filter.py",
     "classname": "SiteFilter",
-    "type": "site-filter",
+    "type": "popup",
     "componentpath": "views/components/search/site-filter",
     "componentname": "site-filter",
     "sortorder": "0",
@@ -18,22 +25,36 @@ details = {
 
 class SiteFilter(BaseSearchFilter):
     def append_dsl(self, search_results_object, permitted_nodegroups, include_provisional):
-        print("APPENDING FPAN DSL")
 
-        try:
-            search_query = Bool()
-            querysting_params = self.request.GET.get(details["componentname"], "")
-            print(querysting_params)
-            # graph_ids = []
-            # for resouceTypeFilter in JSONDeserializer().deserialize(querysting_params):
-            #     graph_ids.append(str(resouceTypeFilter["graphid"]))
-            #
-            # terms = Terms(field="graph_id", terms=graph_ids)
-            # if resouceTypeFilter["inverted"] is True:
-            #     search_query.must_not(terms)
-            # else:
-            #     search_query.filter(terms)
+        original_dsl = search_results_object["query"]._dsl
+        if settings.LOG_LEVEL == "DEBUG":
+            with open(os.path.join(settings.LOG_DIR, "dsl_before_fpan.json"), "w") as output:
+                json.dump(original_dsl, output, indent=1)
 
-            search_results_object["query"].add_query(search_query)
-        except Exception as e:
-            print(e)
+        querystring_params = self.request.GET.get(details["componentname"], "")
+
+        ## this will only be the case for superusers, and the FMSF and FL_BAR accounts.
+        ## in this case no extra filter will be added to the query
+        if querystring_params == "disabled":
+            return
+
+        ## this will be the vast majority of cases
+        elif querystring_params == "enabled":
+            try:
+
+                search_query = apply_advanced_docs_permissions(original_dsl, self.request)
+
+                if not search_query is None:
+                    search_results_object["query"].add_query(search_query)
+
+                if settings.LOG_LEVEL == "DEBUG":
+                    with open(os.path.join(settings.LOG_DIR, "dsl_after_fpan.json"), "w") as output:
+                        json.dump(search_results_object["query"]._dsl, output, indent=1)
+
+            except Exception as e:
+                logger.debug(e)
+
+        ## play it super safe and raise an exception here, which would be caused by
+        ## a problem in the upstream javascript
+        else:
+            raise Exception
