27cdf6979d30cbcee46e36e4b62baecfd2c283af
adam cox <mr.adamcox@gmail.com>
2020-03-20 14:25:05 -0500

    remove obsolete code

--- fpan/urls.py
+++ fpan/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     url(r'^scout-list-download/$', scout.scout_list_download, name='scout_list_download'),
     url(r'^report/(?P<resourceid>%s)$' % uuid_regex, report.FPANResourceReportView.as_view(), name='resource_report'),
     # url(r'^search$', search.FPANSearchView.as_view(), name="search_home"),
-    # url(r'^search/resources$', search.search_results, name="search_results"),
     url(
         r"^mvt/(?P<nodeid>%s)/(?P<zoom>[0-9]+|\{z\})/(?P<x>[0-9]+|\{x\})/(?P<y>[0-9]+|\{y\}).pbf$" % uuid_regex,
         api.MVT.as_view(),
