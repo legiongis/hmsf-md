caeb84776336fbd55e10e2dca35cb9834de4186e
Adam Cox <mr.adamcox@gmail.com>
2021-01-12 12:05:18 -0600

    remove lm models from fpan app - they now live in hms (reversion)

--- fpan/admin.py
+++ fpan/admin.py
@@ -1,23 +1,9 @@
 from django.contrib.gis import admin
-from fpan.models import Region, ManagedArea, ManagerProfile
+from fpan.models import Region, ManagedArea
 
 class ManagedAreaAdmin(admin.GeoModelAdmin):
 
     search_fields = ["name"]
 
-admin.site.register(Region, admin.GeoModelAdmin)
-admin.site.register(ManagedArea, ManagedAreaAdmin)
-
-from fpan.models import Agency, ManagementArea, ManagementAreaType, ManagementAreaGroup
-
-class ManagementAreaGroupAdmin(admin.ModelAdmin):
-    filter_horizontal = ('areas',)
-
-class ManagerProfileAdmin(admin.ModelAdmin):
-    filter_horizontal = ('areas', 'area_groups')
-
-admin.site.register(Agency)
-admin.site.register(ManagementArea, admin.GeoModelAdmin)
-admin.site.register(ManagementAreaType)
-admin.site.register(ManagementAreaGroup, ManagementAreaGroupAdmin)
-admin.site.register(ManagerProfile, ManagerProfileAdmin)
+# admin.site.register(Region, admin.GeoModelAdmin)
+# admin.site.register(ManagedArea, ManagedAreaAdmin)
