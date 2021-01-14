32d2f375ecc073aaa96abf38eaf2d229c02c0e96
Adam Cox <mr.adamcox@gmail.com>
2021-01-14 14:44:57 -0600

    add user_type variable for use in templates

--- fpan/utils/context_processors.py
+++ fpan/utils/context_processors.py
@@ -7,10 +7,19 @@ from .permission_backend import user_is_land_manager, user_is_scout
 
 def user_type(request):
 
+    user_type = "anonymous"
+    if user_is_scout(request.user):
+        user_type = "scout"
+    elif user_is_land_manager(request.user):
+        user_type = "landmanager"
+    elif request.user.is_superuser:
+        user_type = "admin"
+
     return {
         'user_is_state': user_is_land_manager(request.user),
         'user_is_scout': user_is_scout(request.user),
         'user_is_admin': request.user.is_superuser,
+        'user_type': user_type,
     }
 
 def debug(request):
