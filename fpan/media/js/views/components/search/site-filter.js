f06038f5254507ffa4d192e8a7f1c11e263508b1
Adam Cox <mr.adamcox@gmail.com>
2021-01-14 09:37:16 -0600

    always enabled site_filter; migrate get_doc_types

--- fpan/media/js/views/components/search/site-filter.js
+++ fpan/media/js/views/components/search/site-filter.js
@@ -1,26 +1,28 @@
 define([
     'knockout',
     'views/components/search/base-filter',
-    'fpan'
-], function(ko, BaseFilter, FPAN) {
+    // 'fpan'
+], function(ko, BaseFilter) {
     var componentName = 'site-filter';
     return ko.components.register(componentName, {
         viewModel: BaseFilter.extend({
             initialize: function(options) {
                 options.name = 'Site Filter';
                 BaseFilter.prototype.initialize.call(this, options);
-                this.restoreState();
+                // must set something here, otherwise the url param isn't added.
+                this.query()[componentName] = 'enabled';
+                // this.restoreState();
             },
 
-            restoreState: function() {
-                var queryObj = this.query();
-                if (FPAN.full_site_access == "True") {
-                  queryObj[componentName] = 'disabled';
-                } else {
-                  queryObj[componentName] = 'enabled';
-                }
-                this.query(queryObj);
-            },
+            // restoreState: function() {
+            //     var queryObj = this.query();
+            //     if (FPAN.full_site_access == "True") {
+            //       queryObj[componentName] = 'disabled';
+            //     } else {
+            //       queryObj[componentName] = 'enabled';
+            //     }
+            //     this.query(queryObj);
+            // },
 
         }),
         template: { require: 'text!templates/views/components/search/site-filter.htm' }
--- fpan/search/components/site_filter.py
+++ fpan/search/components/site_filter.py
@@ -5,6 +5,8 @@ from arches.app.utils.betterJSONSerializer import JSONDeserializer
 from arches.app.search.elasticsearch_dsl_builder import Bool, Terms, GeoShape, Nested
 from arches.app.search.components.base import BaseSearchFilter
 from arches.app.models.system_settings import settings
+from arches.app.models.models import GraphModel
+from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
 from fpan.utils.search_filter import apply_advanced_docs_permissions
 
 logger = logging.getLogger(__name__)
@@ -33,48 +35,77 @@ class SiteFilter(BaseSearchFilter):
 
         querystring_params = self.request.GET.get(details["componentname"], "")
 
-        ## this will only be the case for superusers, and the FMSF and FL_BAR accounts.
-        ## in this case no extra filter will be added to the query
-        if querystring_params == "disabled":
-            return
+        ## Should always be enabled. If not, raise exception.
+        if querystring_params != "enabled":
+            raise(Exception("Site filter is registered but not shown as enabled."))
 
-        ## this will be the vast majority of cases
-        elif querystring_params == "enabled":
-            try:
+        try:
 
-                search_query = apply_advanced_docs_permissions(original_dsl, self.request)
+            search_query = apply_advanced_docs_permissions(original_dsl, self.request)
 
-                if not search_query is None:
-                    search_results_object["query"].add_query(search_query)
+            if not search_query is None:
+                search_results_object["query"].add_query(search_query)
 
-                # coords = [[[-81.477111876252,29.133006285427328],[-81.47314589490625,28.67123161923965],[-81.06663280694531,28.667751860292498],[-81.07258177896466,29.134738397156326],[-81.477111876252,29.133006285427328]]]
-                # geoshape = GeoShape(
-                #     field="geometries.geom.features.geometry", type="Polygon", coordinates=coords
-                # )
-                #
-                # try:
-                #     self.request.user.ManagerProfile
-                # except Exception as e:
-                #     print(e)
-                #
-                #
-                # spatial_query = Bool()
-                # spatial_query.filter(geoshape)
-                #
-                # search_query = Bool()
-                # search_query.filter(Nested(path="geometries", query=spatial_query))
-                #
-                # search_results_object["query"].add_query(search_query)
-                # print(search_results_object["query"])
+            # coords = [[[-81.477111876252,29.133006285427328],[-81.47314589490625,28.67123161923965],[-81.06663280694531,28.667751860292498],[-81.07258177896466,29.134738397156326],[-81.477111876252,29.133006285427328]]]
+            # geoshape = GeoShape(
+            #     field="geometries.geom.features.geometry", type="Polygon", coordinates=coords
+            # )
+            #
+            # try:
+            #     self.request.user.ManagerProfile
+            # except Exception as e:
+            #     print(e)
+            #
+            #
+            # spatial_query = Bool()
+            # spatial_query.filter(geoshape)
+            #
+            # search_query = Bool()
+            # search_query.filter(Nested(path="geometries", query=spatial_query))
+            #
+            # search_results_object["query"].add_query(search_query)
+            # print(search_results_object["query"])
 
-                if settings.LOG_LEVEL == "DEBUG":
-                    with open(os.path.join(settings.LOG_DIR, "dsl_after_fpan.json"), "w") as output:
-                        json.dump(search_results_object["query"]._dsl, output, indent=1)
+            if settings.LOG_LEVEL == "DEBUG":
+                with open(os.path.join(settings.LOG_DIR, "dsl_after_fpan.json"), "w") as output:
+                    json.dump(search_results_object["query"]._dsl, output, indent=1)
 
-            except Exception as e:
-                logger.debug(e)
+        except Exception as e:
+            print("\n\n")
+            print(e)
+            logger.debug(e)
 
-        ## play it super safe and raise an exception here, which would be caused by
-        ## a problem in the upstream javascript
+
+    def get_doc_types(self, request):
+
+        all_resource_graphids = (
+            GraphModel.objects.exclude(pk=settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID)
+            .exclude(isresource=False)
+            .exclude(isactive=False)
+        ).values_list('graphid', flat=True)
+
+        type_filter = request.GET.get('typeFilter', '')
+        use_ids = []
+
+        if type_filter != '':
+            type_filters = JSONDeserializer().deserialize(type_filter)
+
+            ## add all positive filters to the list of good ids
+            pos_filters = [i['graphid'] for i in type_filters if not i['inverted']]
+            for pf in pos_filters:
+                use_ids.append(pf)
+
+            ## if there are negative filters, make a list of all possible ids and
+            ## subtract the negative filter ids from it.
+            neg_filters = [i['graphid'] for i in type_filters if i['inverted']]
+            if len(neg_filters) > 0:
+                use_ids = [str(i) for i in all_resource_graphids if not str(i) in neg_filters]
+
         else:
-            raise Exception
+            use_ids = [str(i) for i in all_resource_graphids]
+
+        if len(use_ids) == 0:
+            ret = []
+        else:
+            ret = list(set(use_ids))
+        return ret
