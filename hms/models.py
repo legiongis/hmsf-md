539445542da48229ceae10f76583bc1e99f9624f
Adam Cox <mr.adamcox@gmail.com>
2021-02-11 08:03:40 -0600

    add new model to record user X resource access

--- hms/models.py
+++ hms/models.py
@@ -8,6 +8,20 @@ from django.dispatch import receiver
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.gis.geos import MultiPolygon
 
+from arches.app.models.resource import Resource
+from arches.app.models.models import GraphModel
+
+class UserXResourceInstanceAccess(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    resource = models.ForeignKey(
+        Resource,
+        on_delete=models.CASCADE,
+    )
+
 # Create your models here.
 class Scout(User):
     middle_initial = models.CharField(max_length=1)
@@ -146,6 +160,38 @@ class LandManager(models.Model):
     def filter_rules(self):
 
         rules = {"access_level": "", "":""}
+
+    def set_allowed_resources(self):
+        """very confusingly, this method must be called from admin.LandManagerAdmin.save_related().
+        This is because self.save() and post_save here do not yet have the updated versions
+        of the ManyToManyFields (individual_areas and grouped_areas)"""
+
+        from fpan.search.components.site_filter import SiteFilter
+
+        all_resource_graphids = (
+            GraphModel.objects.filter(isresource=True, isactive=True)
+            .exclude(name="Arches System Settings")
+        ).values_list('graphid', flat=True)
+
+        all_resources = []
+        for graphid in [str(i) for i in all_resource_graphids]:
+            try:
+                res_access = SiteFilter().get_allowed_resource_ids(self.user, graphid)
+            except:
+                continue
+            if res_access["access_level"] == "partial_access":
+                all_resources += res_access["id_list"]
+
+        for resourceid in all_resources:
+            res = Resource.objects.get(pk=resourceid)
+            obj, created = UserXResourceInstanceAccess.objects.get_or_create(
+                user=self.user,
+                resource=res
+            )
+
+        for entry in UserXResourceInstanceAccess.objects.filter(user=self.user):
+            if not str(entry.resource.resourceinstanceid) in all_resources:
+                entry.delete()
 
 @receiver(post_save, sender=LandManager)
 def create_user_land_manager(sender, instance, created, **kwargs):
