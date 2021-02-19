02a885f464d32d44802d373da9ba6c6a7b2794ad
Adam Cox <mr.adamcox@gmail.com>
2021-02-19 16:43:40 -0600

    update decorators for more consistent returns

--- fpan/views/resource.py
+++ fpan/views/resource.py
@@ -15,26 +15,26 @@ from arches.app.utils.decorators import (
 from fpan.decorators import can_access_resource_instance
 
 
-@method_decorator(can_edit_resource_instance, name="dispatch")
 @method_decorator(can_access_resource_instance, name="dispatch")
+@method_decorator(can_edit_resource_instance, name="dispatch")
 class FPANResourceListView(ResourceListView):
     pass
 
 
-@method_decorator(can_edit_resource_instance, name="dispatch")
 @method_decorator(can_access_resource_instance, name="dispatch")
+@method_decorator(can_edit_resource_instance, name="dispatch")
 class FPANResourceEditLogView(ResourceEditLogView):
     pass
 
 
-@method_decorator(can_edit_resource_instance, name="dispatch")
 @method_decorator(can_access_resource_instance, name="dispatch")
+@method_decorator(can_edit_resource_instance, name="dispatch")
 class FPANResourceData(ResourceData):
     pass
 
 
-@method_decorator(can_read_resource_instance, name="dispatch")
 @method_decorator(can_access_resource_instance, name="dispatch")
+@method_decorator(can_read_resource_instance, name="dispatch")
 class FPANResourceTiles(ResourceTiles):
     pass
 
@@ -45,13 +45,13 @@ class FPANResourceCards(ResourceCards):
     pass
 
 
-@method_decorator(can_read_resource_instance, name="dispatch")
 @method_decorator(can_access_resource_instance, name="dispatch")
+@method_decorator(can_read_resource_instance, name="dispatch")
 class FPANResourceReportView(ResourceReportView):
     pass
 
 
-@method_decorator(can_read_resource_instance, name="dispatch")
 @method_decorator(can_access_resource_instance, name="dispatch")
+@method_decorator(can_read_resource_instance, name="dispatch")
 class FPANRelatedResourcesView(RelatedResourcesView):
     pass
