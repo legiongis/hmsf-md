5a9a787c5fdafb4dcc03c67d88743f73c5edf4e1
Adam Cox <mr.adamcox@gmail.com>
2021-06-15 13:54:13 -0500

    add try/except for development

--- fpan/utils/context_processors.py
+++ fpan/utils/context_processors.py
@@ -1,13 +1,18 @@
+from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.models import User
 from arches.app.models.models import GraphModel
 from arches.app.models.system_settings import settings
 from arches.app.utils.betterJSONSerializer import JSONSerializer
 
+
 from hms.models import ManagementArea
 
-from fpan.datatypes.management_area import ManagementAreaDataType
-from fpan.datatypes.username import UsernameDataType
+try:
+    from fpan.datatypes.management_area import ManagementAreaDataType
+    from fpan.datatypes.username import UsernameDataType
+except ImportError:
+    pass
 
 from .permission_backend import user_is_land_manager, user_is_scout
 
@@ -18,17 +23,19 @@ def widget_data(request):
 
     if any([True for i in widget_views if i in request.path.split("/")]):
 
-        users = UsernameDataType().get_dropdown_options()
-        management_areas = ManagementAreaDataType().get_dropdown_options()
-
-        return {
-            'lists': {
-                'usernames': users,
-                'managementAreas': management_areas,
+        try:
+            users = UsernameDataType().get_dropdown_options()
+            management_areas = ManagementAreaDataType().get_dropdown_options()
+            return {
+                'lists': {
+                    'usernames': users,
+                    'managementAreas': management_areas,
+                }
             }
-        }
+        except:
+            pass
 
-    return { 'lists': {}, 'requestpath': request.path }
+    return { 'lists': {} }
 
 def user_type(request):
 
