6cfad5aaf9b8cf72a8caf5b78d0b2e2d654a512c
Adam Cox <mr.adamcox@gmail.com>
2021-08-10 15:50:30 -0500

    add and implement new decorator for resource access

--- fpan/decorators.py
+++ fpan/decorators.py
@@ -4,6 +4,7 @@ from django.core.exceptions import PermissionDenied
 from arches.app.models.models import ResourceInstance
 
 from hms.utils import user_can_access_resource
+from fpan.search.components.site_filter import SiteFilter
 
 def user_can_access_resource_instance(function):
     @functools.wraps(function)
@@ -14,6 +15,34 @@ def user_can_access_resource_instance(function):
             raise Http404
 
         if user_can_access_resource(request.user, resourceid):
+            return function(request, *args, **kwargs)
+        else:
+            raise Http404
+
+    return wrapper
+
+def can_access_site_or_report(function):
+    @functools.wraps(function)
+    def wrapper(request, *args, **kwargs):
+
+        resourceid = kwargs.get("resourceid")
+        if resourceid is None:
+            raise Http404
+
+        graphid = str(ResourceInstance.objects.get(pk=resourceid).graph_id)
+        allowed = False
+
+        graph_rules = SiteFilter().compile_rules(request.user, graph=graphid)
+
+        if graph_rules["access_level"] == "full_access":
+            allowed = True
+        elif graph_rules["access_level"] == "no_access":
+            allowed = False
+        else:
+            resids = SiteFilter().get_resource_list_from_es_query(graph_rules, graphid)
+            allowed = resourceid in resids
+
+        if allowed:
             return function(request, *args, **kwargs)
         else:
             raise Http404
--- fpan/views/resource.py
+++ fpan/views/resource.py
@@ -13,46 +13,46 @@ from arches.app.utils.decorators import (
     can_read_resource_instance,
 )
 
-from fpan.decorators import user_can_access_resource_instance
+from fpan.decorators import can_access_site_or_report
 
 
-@method_decorator(user_can_access_resource_instance, name="dispatch")
+@method_decorator(can_access_site_or_report, name="dispatch")
 @method_decorator(can_edit_resource_instance, name="dispatch")
 class FPANResourceListView(ResourceListView):
     pass
 
 
-@method_decorator(user_can_access_resource_instance, name="dispatch")
+@method_decorator(can_access_site_or_report, name="dispatch")
 @method_decorator(can_edit_resource_instance, name="dispatch")
 class FPANResourceEditLogView(ResourceEditLogView):
     pass
 
 
-@method_decorator(user_can_access_resource_instance, name="dispatch")
+@method_decorator(can_access_site_or_report, name="dispatch")
 @method_decorator(can_edit_resource_instance, name="dispatch")
 class FPANResourceData(ResourceData):
     pass
 
 
-@method_decorator(user_can_access_resource_instance, name="dispatch")
+@method_decorator(can_access_site_or_report, name="dispatch")
 @method_decorator(can_read_resource_instance, name="dispatch")
 class FPANResourceTiles(ResourceTiles):
     pass
 
 
-@method_decorator(user_can_access_resource_instance, name="dispatch")
+@method_decorator(can_access_site_or_report, name="dispatch")
 @method_decorator(can_read_resource_instance, name="dispatch")
 class FPANResourceCards(ResourceCards):
     pass
 
 
-@method_decorator(user_can_access_resource_instance, name="dispatch")
+@method_decorator(can_access_site_or_report, name="dispatch")
 @method_decorator(can_read_resource_instance, name="dispatch")
 class FPANResourceReportView(ResourceReportView):
     pass
 
 
-@method_decorator(user_can_access_resource_instance, name="dispatch")
+@method_decorator(can_access_site_or_report, name="dispatch")
 @method_decorator(can_read_resource_instance, name="dispatch")
 class FPANRelatedResourcesView(RelatedResourcesView):
     pass
