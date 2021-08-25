55454d689fb246510a445e628045268fc56fd75b
Adam Cox <mr.adamcox@gmail.com>
2021-08-25 10:48:45 -0500

    remove unused file

--- fpan/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.gis import admin
-from fpan.models import Region, ManagedArea
-
-class ManagedAreaAdmin(admin.GeoModelAdmin):
-
-    search_fields = ["name"]
-
-# admin.site.register(Region, admin.GeoModelAdmin)
-# admin.site.register(ManagedArea, ManagedAreaAdmin)
