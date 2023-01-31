3e3728a748e5091f69e36564b99cdabdbabb8767
Adam Cox <mr.adamcox@gmail.com>
2023-01-31 09:30:52 -0600

    remove extra basemap; add method to update default map layers

--- fpan/management/commands/setup_hms.py
+++ fpan/management/commands/setup_hms.py
@@ -1,5 +1,8 @@
 from django.core import management
 from django.core.management.base import BaseCommand, CommandError
+
+from arches.app.models.models import MapLayer
+
 from fpan.utils.testing import create_mock_landmanagers, create_mock_scout_accounts
 
 class Command(BaseCommand):
@@ -8,10 +11,8 @@ class Command(BaseCommand):
         "overwrites set accounts if they already exist."
 
     def add_arguments(self, parser):
-        parser.add_argument("--landmanagers", action="store_true", default=False,
-            help='specify whether the full complement of state land manager accounts should be created')
-        parser.add_argument("--scouts", action="store_true", default=False,
-            help='specify whether some mock scout accounts should be created')
+        parser.add_argument("--test-accounts", action="store_true", default=False,
+            help='specify whether the mock land manager and scout accounts should be created')
 
     def handle(self, *args, **options):
 
@@ -23,7 +24,11 @@ class Command(BaseCommand):
 
         management.call_command('packages', operation='load_package', source='./pkg', yes=True)
 
-        print("\033[96m-- Load MAP LAYERS --\033[0m")
+        print("\033[96m-- Update MAP LAYERS --\033[0m")
+
+        self.update_map_layers()
+
+        print("\033[96m-- Load extra MAP LAYERS --\033[0m")
 
         management.call_command('loaddata', '1919-coastal-map')
         management.call_command('loaddata', 'slr1-layer')
@@ -57,5 +62,30 @@ class Command(BaseCommand):
         print("\033[96m-- Load MANAGEMENT AREA GROUPS --\033[0m")
         management.call_command('loaddata', 'management-area-groups')
 
-        # create_mock_landmanagers()
-        # create_mock_scout_accounts()
+        if options['test_accounts']:
+            create_mock_landmanagers()
+            create_mock_scout_accounts()
+
+    def update_map_layers(self):
+
+        # rename the default Arches satellite layer
+        try:
+            l = MapLayer.objects.get(name="satellite")
+            l.name = "Satellite"
+            l.icon = "fa fa-globe"
+            l.save()
+        except MapLayer.DoesNotExist:
+            pass
+        # remove the default Arches streets layer
+        try:
+            l = MapLayer.objects.get(name="streets")
+            l.delete()
+        except MapLayer.DoesNotExist:
+            pass
+        # update the icon for the new HMS basemap
+        try:
+            l = MapLayer.objects.get(name="HMS Basemap")
+            l.icon = "fa fa-stumbleupon"
+            l.save()
+        except MapLayer.DoesNotExist:
+            pass
