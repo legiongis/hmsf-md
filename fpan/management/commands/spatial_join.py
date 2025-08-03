74025be79623e68a6ffa341eb78bc40339f2eb1a
Adam Cox <mr.adamcox@gmail.com>
2025-08-03 08:25:13 -0500

    more print statements in command

--- fpan/management/commands/spatial_join.py
+++ fpan/management/commands/spatial_join.py
@@ -5,13 +5,14 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from arches.app.models.models import ResourceInstance
 from arches.app.models.graph import Graph
+from arches.app.models.tile import Tile
 
 from fpan.utils import SpatialJoin
 from fpan.tasks import run_full_spatial_join
 
 class Command(BaseCommand):
 
-    help = 'export all of the FMSF site ids from resources in the database'
+    help = "Performs a spatial join between the management areas and resources"
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -23,7 +24,15 @@ class Command(BaseCommand):
             action="store_true",
         )
         parser.add_argument(
+            '--backfill',
+            action="store_true",
+        )
+        parser.add_argument(
             '--background',
+            action="store_true",
+        )
+        parser.add_argument(
+            '--noinput',
             action="store_true",
         )
 
@@ -42,6 +51,20 @@ class Command(BaseCommand):
                 "Historic Cemetery",
                 "Historic Structure",
             ])
+        elif options["backfill"]:
+            # find all empty FPAN Region nodes and use only these resources
+            resids = []
+            for k, v in settings.SPATIAL_JOIN_GRAPHID_LOOKUP.items():
+                print(k)
+                tiles = Tile.objects.filter(nodegroup_id=v['Nodegroup'])
+                empty_tiles = [i for i in tiles if i.data and not i.data[v["FPAN Region"]]]
+                print(f"resources to join: {len(empty_tiles)}")
+                resids += [i.resourceinstance.pk for i in empty_tiles]
+            resources = ResourceInstance.objects.filter(pk__in=resids)
+            print(f"\ntotal resources: {resources.count()}")
+            if not options["noinput"]:
+                if input("continue? Y/n").lower().startswith("n"):
+                    exit()
         else:
             exit()
 
