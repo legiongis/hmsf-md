27ad84f5ba8ba1c604be861bca125258598e7927
Adam Cox <mr.adamcox@gmail.com>
2026-01-21 14:00:28 -0600

    use postgres15-postgis3.5 in dev

--- scripts/postgis_init_dev.sh
+++ scripts/postgis_init_dev.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/bash
 
-docker run --name postgis13 \
-  -p 5432:5432 \
-  -e POSTGRES_PASSWORD=postgres \
-  -e POSTGRES_HOST_AUTH_METHOD=trust \
-  postgis/postgis:13-3.5-alpine
+docker create --name postgis15 \
+        -p 5432:5432 \
+        -e POSTGRES_PASSWORD=postgres \
+        -e POSTGRES_HOST_AUTH_METHOD=trust \
+        postgis/postgis:15-3.5-alpine
+
