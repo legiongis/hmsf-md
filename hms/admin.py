a1c8ba20e7f70b61c19341d633d9a4a355446449
Adam Cox <mr.adamcox@gmail.com>
2026-04-02 14:35:41 -0500

    update admin display content

--- hms/admin.py
+++ hms/admin.py
@@ -19,6 +19,7 @@ class ScoutProfileInline(admin.StackedInline):
 
 
 class ScoutProfileAdmin(admin.ModelAdmin):
+    list_display = ("username", "interests", "fpan_regions")
     search_fields = ["username", "first_name", "last_name"]
     inlines = (ScoutProfileInline,)
 
@@ -27,22 +28,41 @@ class ScoutProfileAdmin(admin.ModelAdmin):
             return list()
         return super(ScoutProfileAdmin, self).get_inline_instance(request, obj)  # pyright: ignore[reportAttributeAccessIssue]
 
+    def fpan_regions(self, obj):
+        return ", ".join([i.name for i in obj.scoutprofile.fpan_regions2.all()])
+
+    def interests(self, obj):
+        return obj.scoutprofile.site_interest_type
+
 
 admin.site.register(Scout, ScoutProfileAdmin)
 
 
 class LandManagerAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "site_access_mode",
+        "management_agency",
+        "management_area_names",
+        "management_area_group_names",
+    )
+    list_filter = (
+        "site_access_mode",
+        "management_agency",
+    )
     filter_horizontal = ("individual_areas", "grouped_areas")
     search_fields = ("username",)
     exclude = ("username",)
     readonly_fields = ("site_access_rules_formatted", "accessible_sites_formatted")
 
+    def management_area_names(self, obj):
+        return ", ".join([i.name for i in obj.individual_areas.all()])
+
+    def management_area_group_names(self, obj):
+        return ", ".join([i.name for i in obj.grouped_areas.all()])
+
 
 admin.site.register(LandManager, LandManagerAdmin)
-
-## this is registered so that a new Land Manager can be made directly from the
-## Land Manager Profile interface, using the green + button.
-# admin.site.register(LandManager)
 
 
 class ManagementAreaAdmin(admin.ModelAdmin):
@@ -59,7 +79,11 @@ class ManagementAreaAdmin(admin.ModelAdmin):
 
 
 class ManagementAreaGroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "management_area_names")
     filter_horizontal = ("areas",)
+
+    def management_area_names(self, obj):
+        return ", ".join([i.name for i in obj.areas.all()])
 
 
 admin.site.register(ManagementArea, ManagementAreaAdmin)
