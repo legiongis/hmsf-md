d4b25f091a6c2eabe81f5973a2b5e333ef69e492
Adam Cox <mr.adamcox@gmail.com>
2026-04-02 14:15:35 -0500

    make note optional on management area group

--- hms/models.py
+++ hms/models.py
@@ -698,7 +698,7 @@ class ManagementAreaGroup(models.Model):
 
     name = models.CharField(max_length=100)
     areas = models.ManyToManyField(ManagementArea)
-    note = models.CharField(max_length=255)
+    note = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
