a1f32e234433dbabab0b3e70f20cf68262f275f1
Adam Cox <mr.adamcox@gmail.com>
2026-04-02 09:00:10 -0500

    remove unused custom widgets

--- fpan/context_processors.py
+++ fpan/context_processors.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.contrib.auth.models import User
-from arches.app.models.system_settings import settings
 
 from hms.models import (
     ManagementArea,
@@ -10,115 +9,49 @@ from hms.models import (
 )
 from fpan.search.components.rule_filter import RuleFilter
 
+
 def debug(request):
     return {
-        'debug':settings.DEBUG,
-        'plausible_site_domain': settings.PLAUSIBLE_SITE_DOMAIN,
-        'plausible_embed_link': settings.PLAUSIBLE_EMBED_LINK,
+        "debug": settings.DEBUG,
+        "plausible_site_domain": settings.PLAUSIBLE_SITE_DOMAIN,
+        "plausible_embed_link": settings.PLAUSIBLE_EMBED_LINK,
     }
 
-def make_widget_data():
-
-    all_areas = ManagementArea.objects.all()
-    general_areas = all_areas.exclude(category__name="FPAN Region").exclude(category__name="County")
-    general_areas = [{
-        "id": str(i[0]),
-        "selected": "false",
-        "text": i[1]
-    } for i in general_areas.order_by("display_name").values_list("pk", "display_name") ]
-
-    fpan_regions = [{
-        "id": str(i[0]),
-        "selected": "false",
-        "text": i[1]
-    } for i in all_areas.filter(category__name="FPAN Region").values_list("pk", "name") ]
-
-    counties = [{
-        "id": str(i[0]),
-        "selected": "false",
-        "text": i[1]
-    } for i in all_areas.filter(category__name="County").values_list("pk", "name") ]
-
-    agencies = [{
-        "id": str(i[0]),
-        "selected": "false",
-        "text": i[1]
-    } for i in ManagementAgency.objects.all().values_list("pk", "name") ]
-
-    usernames = [{
-        "id": str(i[0]),
-        "selected": "false",
-        "text": i[1]
-    } for i in User.objects.all().order_by("username").values_list("pk", "username")]
-
-    return {
-        "lists" : {
-            "usernames": usernames,
-            "generalAreas": general_areas,
-            "fpanRegions": fpan_regions,
-            "managementAgencies": agencies,
-            "counties": counties,
-        }
-    }
 
 def widget_data(request):
 
     # only collect this information for a few specific views
     widget_views = ["resource", "graph_designer", "report", "add-resource"]
 
-    data = { 'lists': {} }
+    data = {"lists": {}}
     if any([True for i in widget_views if i in request.path.split("/")]):
-
-        all_areas = ManagementArea.objects.all()
-        general_areas = all_areas.exclude(category__name="FPAN Region").exclude(category__name="County")
-        general_areas = [{
-            "id": str(i[0]),
-            "selected": "false",
-            "text": str(i[1])
-        } for i in general_areas.order_by("display_name").values_list("pk", "display_name") ]
-
-        fpan_regions = [{
-            "id": str(i[0]),
-            "selected": "false",
-            "text": i[1]
-        } for i in all_areas.filter(category__name="FPAN Region").values_list("pk", "name") ]
-
-        counties = [{
-            "id": str(i[0]),
-            "selected": "false",
-            "text": str(i[1])
-        } for i in all_areas.filter(category__name="County").values_list("pk", "name") ]
-
-        agencies = [{
-            "id": str(i[0]),
-            "selected": "false",
-            "text": str(i[1])
-        } for i in ManagementAgency.objects.all().values_list("pk", "name") ]
-
-        usernames = [{
-            "id": str(i[0]),
-            "selected": "false",
-            "text": str(i[1])
-        } for i in User.objects.all().order_by("username").values_list("pk", "username")]
+        usernames = [
+            {"id": str(i[0]), "selected": "false", "text": str(i[1])}
+            for i in User.objects.all()
+            .order_by("username")
+            .values_list("pk", "username")
+        ]
 
         data["lists"]["usernames"] = usernames
-        data["lists"]["generalAreas"] = general_areas
-        data["lists"]["fpanRegions"] = fpan_regions
-        data["lists"]["managementAgencies"] = agencies
-        data["lists"]["counties"] = counties
 
     return data
+
 
 def management_area_importer_configs(request):
 
     return {
-        "ma_group_opts": ManagementAreaGroup.objects.all().order_by("name").values("id", "name"),
-        "ma_category_opts": ManagementAreaCategory.objects.all().order_by("name").values("id", "name"),
-        "ma_agency_opts": ManagementAgency.objects.all().order_by("name").values("code", "name"),
-        "ma_level_opts": ManagementArea._meta.get_field('management_level').choices,
+        "ma_group_opts": ManagementAreaGroup.objects.all()
+        .order_by("name")
+        .values("id", "name"),
+        "ma_category_opts": ManagementAreaCategory.objects.all()
+        .order_by("name")
+        .values("id", "name"),
+        "ma_agency_opts": ManagementAgency.objects.all()
+        .order_by("name")
+        .values("code", "name"),
+        "ma_level_opts": ManagementArea._meta.get_field("management_level").choices,
     }
 
+
 def rule_filter_html(request):
-    return {
-        "rule_filter_html": RuleFilter().generate_html(request.user)
-    }
+    return {"rule_filter_html": RuleFilter().generate_html(request.user)}
