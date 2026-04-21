785b57759f894215ab4722732947924f9230cfb0
Adam Cox <mr.adamcox@gmail.com>
2026-04-21 16:18:24 -0500

    update rule-filter js component to force application

--- fpan/media/js/views/components/search/rule-filter.js
+++ fpan/media/js/views/components/search/rule-filter.js
@@ -5,21 +5,35 @@ define([
     'templates/views/components/search/rule-filter.htm'
 ], function(ko, arches, BaseFilter, ruleFilterTemplate) {
     var componentName = 'rule-filter';
-    return ko.components.register(componentName, {
-        viewModel: BaseFilter.extend({
-            initialize: async function(options) {
-                options.name = 'Rule Filter';
-                BaseFilter.prototype.initialize.call(this, options);
-                const response = await fetch(arches.urls.api_search_component_data + componentName);
+    const viewModel = BaseFilter.extend({
+        initialize: async function(options) {
+
+            options.name = 'Rule Filter';
+            BaseFilter.prototype.initialize.call(this, options);
+
+            const response = await fetch(arches.urls.api_search_component_data + componentName);
                 if (response.ok) {
                     const data = await response.json();
                     this.rule_filter_html(data.rule_filter_html);
                 } else {
                     this.rule_filter_html('Failed to fetch rule-filter summary');
                 }
-            },
-            rule_filter_html: ko.observable(),
-        }),
-        template: ruleFilterTemplate
+
+            this.searchFilterVms[componentName](this);
+            this.restoreState();
+        },
+
+        rule_filter_html: ko.observable(),
+
+        restoreState: function() {
+            var query = this.query();
+            query[componentName] = "enabled";
+            this.query(query);
+        }
+
+    });
+    return ko.components.register(componentName, {
+        viewModel: viewModel,
+        template: ruleFilterTemplate,
     });
 });
